a13af07f0bfa43131f4b003beb88689cd35f5d48
Romain Monsterlet <romain.monsterlet@gmail.com>
2019-06-12 18:40:12 +0200

    lexique and a-propos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
+import { GlossaryComponent } from './components/glossary/glossary.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
-
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppMaterialModule } from './app.material.module';
-
 import * as moment from 'moment';
+
 moment.locale('fr');
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'lexique', component: GlossaryComponent },
+  { path: 'a-propos', component: AboutComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    GlossaryComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,7 +34,11 @@ moment.locale('fr');
     MDBBootstrapModule.forRoot(),
     FormsModule,
     ReactiveFormsModule,
-    AppMaterialModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    AppMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,134 @@
+import { CONFIG, APP_CONSTANTS } from './../../constants';
+import * as moment from 'moment';
+import { ActivatedRoute } from '@angular/router';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+import * as sha256 from 'sha256';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+// import * as RSS from 'rss';
+
+@Component({
+  selector: 'lpdc-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  @ViewChild('fileInput') fileInput: ElementRef;
+
+  public episodes = [];
+  public uploadForm: FormGroup;
+  public file: File;
+
+  public showUpload = false;
+
+  public upload$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+  ) {
+    this.fetchEpisodes();
+
+    this.uploadForm = this.formBuilder.group({
+      title: '',
+      description: '',
+      filename: '',
+      password: '',
+      userFile: null
+    });
+  }
+
+  public ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.showUpload = params.admin === 'true';
+    });
+  }
+
+  public fetchEpisodes() {
+    this.episodes = [];
+    firebase.firestore().collection(APP_CONSTANTS.FIRESTORE_COLLECTION).get()
+      .then(({ docs }) => docs.map(async (doc) => {
+        const { title, description, fileName, date } = doc.data();
+        this.episodes.push({
+          id: doc.id,
+          title,
+          description,
+          fileName,
+          date,
+          link: await firebase.storage().ref().child(`${APP_CONSTANTS.STORAGE_FOLDER}/${fileName}`).getDownloadURL(),
+        });
+      }))
+      .then(() => this.updateRss());
+  }
+
+  public updateRss() {
+		/*const feed = new RSS({
+			title: 'La Pointe Du Cul',
+			description: 'La Pointe Du Cul',
+			feed_url: 'https://cdn-lapointeducul.firebaseapp.com/rss.xml',
+			site_url: 'https://cdn-lapointeducul.firebaseapp.com',
+			image_url: 'https://cdn-lapointeducul.firebaseapp.com/assets/jpg.png',
+			managingEditor: 'Corzeam',
+			webMaster: 'monster',
+			copyright: '2019 lpdc',
+			language: 'fr',
+			ttl: '60',
+		});
+		this.episodes.forEach(episode => {
+			feed.item({
+				title: episode.title,
+				description: episode.description,
+				url: episode.link, // link to the item
+				author: 'Corzeam',
+				date: episode.date,
+				enclosure: { url: episode.link, file: episode.fileName },
+			});
+		})
+		const xml = feed.xml();
+		debugger;
+		return xml;*/
+  }
+
+  public onSelectFile(event) {
+    if (event.target.files && event.target.files.length > 0) {
+      this.file = event.target.files[0];
+      this.uploadForm.get('filename').setValue(this.file.name);
+    }
+  }
+
+  public selectFile(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  public sendFile() {
+    const title = this.uploadForm.get('title').value;
+    const description = this.uploadForm.get('description').value;
+    const password = this.uploadForm.get('password').value;
+    const fileName = this.file.name;
+
+    if (!title || !fileName.endsWith('.mp3') || this.file.size > 500000000 || sha256.x2(password) !== CONFIG.UPLOAD) {
+      return; // > 500 Mo
+    }
+    this.upload$.next(true);
+    firebase.storage().ref().child(`${APP_CONSTANTS.STORAGE_FOLDER}/${fileName}`)
+      .put(this.file, {
+        contentType: 'audio/mp3',
+      })
+      .then(() => {
+        this.fetchEpisodes();
+        this.upload$.next(false);
+      });
+    const episodesRef = firebase.firestore().collection(APP_CONSTANTS.FIRESTORE_COLLECTION);
+    episodesRef.doc(episodesRef.doc().id).set({
+      episode: this.episodes.length,
+      title,
+      description,
+      fileName,
+      date: moment().format('L'),
+    });
+  }
+}
