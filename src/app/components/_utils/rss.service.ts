5d89501376e9201e68fe518f9e494b22988d8913
rmonsterlet <romain.monsterlet@gmail.com>
2019-09-19 18:03:36 +0200

    RSS spotify reformat

--- src/app/components/_utils/rss.service.ts
+++ src/app/components/_utils/rss.service.ts
@@ -5,12 +5,12 @@ import * as moment from 'moment';
 export class RssService {
 
   title = 'La Pointe Du Cul';
-  url = 'https://cdn-lapointeducul.firebaseapp.com';
+  url = 'https://lapointeducul.fr';
   avatar = this.encode('https://firebasestorage.googleapis.com/v0/b/cdn-lapointeducul.appspot.com/o/assets%2Flpdc_avatar_2.png?alt=media&token=28a8ea9d-5b10-42d5-b4ec-fa62fda2a97f');
   order = 0;
 
   public getRss(episodes) {
-    return `<?xml version="1.0" encoding="utf-8"?>
+    const rss = `<?xml version="1.0" encoding="utf-8"?>
     <rss xmlns:media="http://search.yahoo.com/mrss/"
           xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
           xmlns:dcterms="http://purl.org/dc/terms/"
@@ -22,10 +22,10 @@ export class RssService {
       <title>${this.title}</title>
       <link>${this.url}</link>
       <description>La pointe du cul c’est un podcast qui parle de cul. Plus précisément, de pratiques sexuelles dites alternatives, hors des sentiers battus. 
-      Pour autant, ce n’est pas un podcast prêchant l’élitisme d’une sexualité plus déviante que la moyenne. 
-      C’est un podcast qui essaie de montrer à quel point ces pratiques “différentes” ne sont ni plus ni moins valables que les autres, 
-      que les gens qui les pratiquent ne sont ni plus ni moins “normaux” que les autres. C’est un podcast à visée pédagogique et désacralisante, son but est d’explorer la sexualité,
-      d’aller dans les recoins, de montrer son incroyable diversité.</description>
+Pour autant, ce n’est pas un podcast prêchant l’élitisme d’une sexualité plus déviante que la moyenne. 
+C’est un podcast qui essaie de montrer à quel point ces pratiques “différentes” ne sont ni plus ni moins valables que les autres, 
+que les gens qui les pratiquent ne sont ni plus ni moins “normaux” que les autres. C’est un podcast à visée pédagogique et désacralisante, son but est d’explorer la sexualité,
+d’aller dans les recoins, de montrer son incroyable diversité. ${this.url}</description>
       <language>fr</language>
       <itunes:author>Raphaël Guillemot</itunes:author>
       <itunes:owner>
@@ -57,30 +57,39 @@ export class RssService {
       </channel>
     </rss>
     `;
+    return rss;
   }
 
   private getEpisodeRss(episode) {
+    const title = this.encode(episode.title);
+    const description = this.encode(episode.description, 'podcast-description');
+    const link = this.encode(episode.link);
+    const pubDate = moment(episode.date, 'DD/MM/YYYY').toDate().toUTCString();
     const item = `  <item>
-        <guid isPermaLink="false">${this.encode(episode.id)}</guid>
+        <guid isPermaLink="false">${episode.id}</guid>
         <author>lapointeducul@gmail.com (Raphaël Guillemot)</author>
-        <title>${this.encode(episode.title)}</title>
+        <title>${title}</title>
         <link>${this.url}</link>
-        <media:content type="audio/mpeg" url="${this.encode(episode.link)}" />
-        <description>${this.encode(episode.description)}</description>
+        <media:content type="audio/mpeg" url="${link}" />
+        <description>${description}</description>
         <itunes:explicit>yes</itunes:explicit>
         <itunes:image href="${this.avatar}" />
-        <pubDate>${moment(episode.date, 'DD/MM/YYYY').toDate().toUTCString()}</pubDate>
+        <pubDate>${pubDate}</pubDate>
         <itunes:order>${++this.order}</itunes:order>
         <itunes:duration>${episode.duration}</itunes:duration>
-        <enclosure url="${this.encode(episode.link)}" length="${episode.duration}" type="audio/mpeg" />
+        <enclosure url="${link}" length="${episode.duration}" type="audio/mpeg" />
       </item>
     `;
     return item;
   }
 
-  private encode(str) {
+  private encode(str, extractClass?) {
     const p = document.createElement('p');
     p.textContent = str;
+    if (extractClass) {
+      p.innerHTML = str;
+      return this.encode(p.getElementsByClassName(extractClass)[0].innerHTML);
+    }
     return p.innerHTML;
   }
 }
